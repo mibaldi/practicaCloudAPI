9864ceb7a37d390a486d5e0befcbb333588d97ad
mikelbalducieldiaz <mikelbalducieldiaz@192.168.0.18>
2015-11-09 17:26:48 +0100

    Creadas las rutas de usuario

--- app/controllers/TareasController.java
+++ app/controllers/TareasController.java
@@ -10,5 +10,20 @@ public class TareasController extends Controller {
     public Result index() {
         return ok("hola");
     }
+    public Result getUser(String nombre){
+    	return ok("existe "+nombre);
+    }
+    public Result listaUsuarios(){
+    	return ok("lista usuarios");
+    }
+    public Result createUser() {
+        return ok("usuario creado");
+    }
+    public Result update(String nombre){
+    	return ok("modificado "+nombre);
+    }
+    public Result delete(String nombre){
+    	return ok("borrado "+nombre);
+    }
 
 }
